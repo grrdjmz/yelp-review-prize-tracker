ddcf1447e82d93066536b54ae9c0c05cf5ccd7e1
grrdjmz <grrdjmz@gmail.com>
2025-09-03 19:42:06 -0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+    serverActions: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.supabase.co',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.supabase.in',
+      },
+    ],
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+};
+
+module.exports = nextConfig;
